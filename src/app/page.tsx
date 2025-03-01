8de6e406279d786d326963438f132302780be649
Timothee <preault@et.esiea.fr>
2025-03-01 14:55:31 +0100

    Suggested Users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
-import Image from "next/image";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
@@ -14,7 +15,9 @@ export default async function Home() {
         {user ? <CreatePost /> : null}
       </div>
 
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">WhoToFollow</div>
-  </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
+    </div>
   );
 }
