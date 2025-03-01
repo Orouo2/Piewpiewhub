5675d526cdba4e6221fab3c700588bafde95f887
Timothee <preault@et.esiea.fr>
2025-03-01 14:33:02 +0100

    Creating Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/CreatePost";
 import Image from "next/image";
 
 export default async function Home() {
+  const user = await currentUser();
 
   return (
-    <div className="m-4">
-      <h1>home page content</h1>
-    </div>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">WhoToFollow</div>
+  </div>
   );
 }
